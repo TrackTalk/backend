4ab2edf346063b4cab0c8fd31d5693fcec3c1933
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2024-03-09 00:27:53 -0500

    feat: created user model

---
+++ db/models/user.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require("sequelize");
+const db = require("../db");
+
+const User = db.define("user", {
+    userName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,  
+        allowNull: false,
+    },
+    profilePicUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    loginMthd: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    currentlyListeningId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    spotifyProfileId: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }
+});
+module.exports = User;
