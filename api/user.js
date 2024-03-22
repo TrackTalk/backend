05d24911a909eabfd67d7283375f1be822f4c814
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2024-03-22 01:04:52 -0400

    feat: create initial api route set up in index.js for user. feat: user initial route set up

---
+++ api/user.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+
+//root is localhost:8000/api/user
+
+module.exports = router;
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const setupMiddleWare = (app) => {
 const syncDB = () => db.sync().then(()=>{console.log("Resync DB")});
 
 const setupRoutes = (app) => {
-    //app.use("/api", require("./api"));
+    app.use("/api", require("./api"));
     app.get("/", (req, res, next) => {
         res.send("Hit home");
     });
