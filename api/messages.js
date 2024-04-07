841d9c729ae26fa9fc4af994ed5d88c62536d4e5
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2024-04-07 02:07:44 -0400

    feat: added endpoints for getting single conversation including messages. feat: added endpoint for getting all conversation of the user

--- api/messages.js
+++ api/messages.js
@@ -5,10 +5,10 @@ const {Op}  = require("sequelize");
 
 //root is localhost:8000/api/messages
 
-router.post("/send/:user2Id", checkJWT, async (req, res, next) => {
+router.post("/send/:userId", checkJWT, async (req, res, next) => {
     try {
         const user1Id = parseInt(req.userData.userId);
-        const {user2Id} = parseInt(req.params);
+        const user2Id = parseInt(req.params.userId);
         const {text} = req.body;
 
         let conversation = await Conversation.findOne({
@@ -36,6 +36,51 @@ router.post("/send/:user2Id", checkJWT, async (req, res, next) => {
 
         res.status(201).json(newMessage);
         
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/get/:userId", checkJWT, async (req, res, next) => {
+    try {
+        const user2Id = parseInt(req.params.userId);
+        const user1Id = parseInt(req.userData.userId);
+        if(!user2Id) return res.status(400).send("User 2 id is required");
+        if(!user1Id) return res.status(400).send("User token is invalid");
+        const conversation = await Conversation.findOne({
+            where: {
+                [Op.or]: [
+                    {participants: [user1Id, user2Id]},
+                    {participants: [user2Id, user1Id]}
+                ]
+            },
+            include: Message
+        });
+        if(!converasation) return res.status(404).send("No such conversation found");
+        res.status(200).json(conversation.messages);
+
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/all", checkJWT, async(req, res, next) => {
+    try {
+        const userId = parseInt(req.userData.userId);
+        if(!userId) return res.status(403).send("Unauthorized User");
+        const conversations = await Conversation.findAll({
+            where:{
+                [Op.or]: [
+                    {user1Id: userId},
+                    {user2Id: userId}
+                ]
+            },
+            include: {
+                all: true
+            }
+        });
+        if(!conversations) return res.status(404).send("Conversations not Found");
+        res.status(200).json(conversations);
     } catch (error) {
         next(error);
     }
